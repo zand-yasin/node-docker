4736699554b98c4c4f34c7f4d83ee49ec22322ae
zand-yasin <zand.kurdy@gmail.com>
2023-06-22 08:30:23 +0300

    Implementing console rection

--- express/index.js
+++ express/index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const process = require('process')
 const app = express()
 
+process.on('SIGINT', ()=>{
+    console.log('Application is being interrupted...')
+    process.exit(0)
+})
+process.on('SIGTERM', ()=>{
+    console.log('Application is being terminated...')
+    process.exit(0)
+})
+
 app.get('/', function (req,res){
     res.send('This Express app was executed by Node.js')
 })
